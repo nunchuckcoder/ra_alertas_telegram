0a5cd2c065800125412e1167785cf2db3f48df88
Mikey <osvaldo.cipriano@gmail.com>
2025-09-23 14:13:27 +0100

    Add files via upload

---
+++ locais.py
@@ -0,0 +1,21 @@
+# ================================================================================ #
+#                                                                                  #
+# Ficheiro:      locais.py                                                         #
+# Autor:         NunchuckCoder                                                     #
+# Versão:        1.0                                                               #
+# Data:          Julho 2025                                                        #
+# Descrição:     Dicionários com distritos e localidades de Portugal e arquipélagos#
+#                usados para consultas de previsão meteorológica na API do IPMA.   #
+# Licença:       MIT License                                                       #
+#                                                                                  #
+# ================================================================================ #
+
+# Dicionário com os distritos: id → nome
+ID_LOCAL_TO_NAME = {
+
+}
+
+# Dicionário com as localidades agrupadas por distrito
+LOCAIS_POR_DISTRITO = {
+    
+}
